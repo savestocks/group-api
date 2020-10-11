31963600a2d6a1a8a0a8bb08ef4f263494e87978
Anderson Lira <anderson.lira@celfocus.com>
2020-10-11 13:50:19 +0100

    authorization added

--- controller/router.go
+++ controller/router.go
@@ -1,6 +1,11 @@
 package controller
 
 import (
+	"encoding/base64"
+	"net/http"
+	"os"
+	"strings"
+
 	"github.com/andersonlira/group-api/config"
 	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
@@ -20,6 +25,9 @@ func MapRoutes(e *echo.Echo) {
 		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.POST, echo.DELETE, echo.OPTIONS},
 	}))
 
+	e.Use(BasicAuth)
+
+
 	g.GET("/group", GetGroupList)
 	g.GET("/group/:id", GetGroupByID)
 	g.POST("/group", SaveGroup)
@@ -27,4 +35,28 @@ func MapRoutes(e *echo.Echo) {
 	g.DELETE("/group/:id", DeleteGroup)
 	g.GET("/health", CheckHealth)
 	g.GET("/info", GetInfo)
+}
+
+// Process is the middleware function to enabled options
+func BasicAuth(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if c.Request().Method == echo.OPTIONS || c.Request().Method == echo.HEAD {
+			next(c)
+			return nil
+		}
+        auth := strings.SplitN(c.Request().Header.Get("Authorization"), " ", 2)
+
+        if len(auth) != 2 || auth[0] != "Basic" {
+            return nil
+        }
+
+        payload, _ := base64.StdEncoding.DecodeString(auth[1])
+        pair := strings.SplitN(string(payload), ":", 2)
+
+        if len(pair) == 2 && pair[0] == os.Getenv("apikey") && pair[1] == os.Getenv("apisecret") {
+			next(c)
+            return nil
+		}
+		return c.JSON(http.StatusUnauthorized, nil)
+	}
 }
